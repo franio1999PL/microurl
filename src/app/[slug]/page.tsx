3880441590c0af1f3b2af2392f4119652c2e9f0f
Franciszek Sikora <sikorafranek@gmail.com>
2023-09-06 22:11:22 +0200

    added geo

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,48 +1,54 @@
-import { redirect } from 'next/navigation'
-import { revalidatePath } from 'next/cache'
-
-import prisma from '@/lib/db'
+'use client'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 type Props = {
   params: { slug: string }
 }
 
-export default async function page ({ params }: Props) {
+export default function page ({ params }: Props) {
+  const [longUrl, setLongUrl] = useState('')
+  const router = useRouter()
+
   const { slug } = params
 
-  const link = await prisma.shortUrl
-    .findFirst({
-      where: {
-        slug
-      }
-    })
-    .finally(() => prisma.$disconnect())
-
-  if (link !== null) {
-    const addAnalytics = await prisma.shortUrl.update({
-      where: {
-        slug
-      },
-      data: {
-        clicks: link?.clicks + 1
-      }
-    })
-    revalidatePath('/dashboard/links')
-    prisma.$disconnect()
-    redirect(link.longUrl)
+  const redirect = (url: string) => {
+    router.push
   }
 
-  const res = await fetch(`${process.env.MAIN_URL}/api/endpoint`, {
-    method: 'GET'
-  }).then(res => res.json())
+  const test = async () => {
+    await fetch(`http://localhost:3000/api/endpoint?slug=${slug}`, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(res => {
+        setLongUrl(res.longUrl)
+        window.location.href = String(res.longUrl)
+      })
+      .catch(err => console.log(err))
+    // .finally(() => (window.location.pathname = String(longUrl)))
+  }
+
+  useEffect(() => {
+    const time = setTimeout(async () => {
+      await test()
+      console.log('chuj')
+
+      console.log('first')
+      redirect(longUrl)
+    }, 1)
+
+    return clearTimeout('time')
+  }, [])
 
   return (
     <div>
       <div>My Post: {params.slug}</div>
-      <div>City: {res ? res.city : ''}</div>
+      <div>{longUrl}</div>
+      {/* <div>City: {res ? res.city : ''}</div>
       <div>Country: {res ? res.country : ''}</div>
       <div>Country Region: {res ? res.countryRegion : ''}</div>
-      <div>region: {res ? res.region : ''}</div>
+      <div>region: {res ? res.region : ''}</div> */}
     </div>
   )
 }
--- src/app/api/endpoint/route.ts
+++ src/app/api/endpoint/route.ts
@@ -1,12 +1,69 @@
 import { NextResponse as res } from 'next/server'
-
+import { revalidatePath } from 'next/cache'
 
 import { geolocation } from '@vercel/edge';
+import prisma from '@/lib/db';
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url)
-    const { city, country, countryRegion, region } = geolocation(req);
+    const { city, country } = geolocation(req);
     const apiKey = searchParams.get('apiKey')
+    const slug = searchParams.get('slug')
 
-    return res.json({ city: city, country, countryRegion, region })
+    if (slug === null) {
+        return res.json({
+            error: {
+                message: 'Slug jest wymagany!'
+            }
+        })
+    }
+
+    const link = await prisma.shortUrl
+        .findFirst({
+            where: {
+                slug
+            }
+        })
+        .finally(() => prisma.$disconnect())
+
+    if (link !== null) {
+        const addAnalytics = await prisma.shortUrl.update({
+            where: {
+                slug
+            },
+            data: {
+                clicks: link?.clicks + 1
+            }
+        }).finally(() => {
+            revalidatePath('/dashboard/links')
+            prisma.$disconnect()
+        })
+
+        const addGeolocation = await prisma.linkClicks.create({
+            data: {
+                linkId: link.id,
+                country: country,
+                city: city
+            }
+        }).finally(() => {
+            // add revalidate path
+            prisma.$disconnect()
+        })
+
+        console.log(addGeolocation)
+
+        return res.json({
+            longUrl: link.longUrl,
+        })
+    }
+
+    if (link === null) {
+        return res.json({
+            error: {
+                message: 'Nie ma takiego linku!'
+            }
+        })
+    }
+
+    // return res.json({ city: city, country, countryRegion, region })
 }

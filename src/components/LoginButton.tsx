2ddeb96e8492add685ff3d7840f5a0bccf3b9205
Franciszek Sikora <sikorafranek@gmail.com>
2023-09-06 16:44:28 +0200

    fixed with google chrome issues with security

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,5 +3,13 @@ import { Button } from './ui/button'
 import { signIn } from 'next-auth/react'
 
 export default function LoginButton () {
-  return <Button onClick={() => signIn('discord')}>Zaloguj</Button>
+  return (
+    <Button
+      onClick={() =>
+        signIn('discord', { redirect: true, callbackUrl: '/dashboard' })
+      }
+    >
+      Zaloguj
+    </Button>
+  )
 }

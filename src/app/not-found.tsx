32e0604baa635b0638730c2d7bcc06de0ecd2437
Franciszek Sikora <sikorafranek@gmail.com>
2023-09-07 03:18:11 +0200

    changed font size in not found page file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,9 +9,12 @@ export default function loading () {
         <GridLoader color='#000000' />
       </div>
       <div className='flex flex-col items-center justify-center gap-2'>
-        <h1 className='text-center text-2xl text-black font-semibold p-16'>
-          Błąd 404! Podana strona nie istnieje!
+        <h1 className='text-center text-4xl text-black font-semibold px-16 pt-16 pb-4'>
+          Błąd 404!
         </h1>
+        <p className='text-center text-2xl text-black font-semibold p-4'>
+          Podana strona nie istnieje!
+        </p>
         <Link
           className='text-black underline text-center text-lg p-8'
           href={'/'}
